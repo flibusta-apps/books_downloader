d93c16ffe84567df21c2e3de8f96dd0be19ff9b7
Kurbanov Bulat <kurbanovbul@gmail.com>
2022-02-12 13:35:46 +0300

    Add metrics

--- src/core/app.py
+++ src/core/app.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 
+from prometheus_fastapi_instrumentator import Instrumentator
+
 from app.views import router
 
 
@@ -7,5 +9,7 @@ def start_app() -> FastAPI:
     app = FastAPI()
 
     app.include_router(router)
+
+    Instrumentator().instrument(app).expose(app, include_in_schema=True)
 
     return app
