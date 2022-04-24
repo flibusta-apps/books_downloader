2855f446b36d865f9cb8aaa8823ce35ad8482f80
Bulat Kurbanov <kurbanovbul@gmail.com>
2022-04-24 15:52:45 +0300

    Add sentry

--- src/core/app.py
+++ src/core/app.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 
 from prometheus_fastapi_instrumentator import Instrumentator
+import sentry_sdk
 
 from app.views import router, healthcheck_router
+from core.config import env_config
+
+
+sentry_sdk.init(
+    env_config.SENTRY_DSN,
+)
 
 
 def start_app() -> FastAPI:
