55e8978464d2a3718f92bad7eb61ac038cc12d94
Bulat Kurbanov <kurbanovbul@gmail.com>
2023-08-10 00:57:41 +0200

    Add sentry

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ pub mod services;
 use std::net::SocketAddr;
 use tracing::info;
 
-use crate::views::get_router;
+use crate::{views::get_router, config::CONFIG};
 
 
 #[tokio::main]
@@ -14,6 +14,8 @@ async fn main() {
         .with_target(false)
         .compact()
         .init();
+
+    let _guard = sentry::init(CONFIG.sentry_dsn.clone());
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
 
